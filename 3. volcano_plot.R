17aed446784677f29cf4d433f3b2ff4e23dde7a3
qinzhang0824 <52529819+qinzhang0824@users.noreply.github.com>
2026-01-18 11:27:34 +0800

    Update 3. volcano_plot.R

--- 3. volcano_plot.R
+++ 3. volcano_plot.R
@@ -7,7 +7,7 @@ library(ggthemes)
 library(TwoSampleMR)
 library(data.table)
 
-mr.or <-fread('mr_result_exposure.cis-pQTLs_protein_outcome.finngen.R12.bile.duct.cancer_addOR.xls',header = T)
+mr.or <-fread('final results/Discovery cohort/mr_result_exposure.cis-pQTLs_protein_outcome.finngen.R12.bile.duct.cancer_addOR.xls',header = T)
 mr.or <- mr.or[mr.or$method!="Weighted median",]
 mr.or <- mr.or[mr.or$method!="MR Egger",]
 
@@ -24,7 +24,6 @@ sig.res <- res %>% mutate(group=case_when(
 
 
 sig.res <- sig.res %>% mutate(group=factor(group,levels = c("Positively associated","Negatively associated","Not significant")))
-
 
 label <-subset(sig.res$id.exposure,sig.res$pval < (0.05/1178))
 label <-subset(sig.res$id.exposure,sig.res$id.exposure %in% c("NCAN"))
@@ -50,9 +49,9 @@ p <- ggscatter(sig.res,
   labs(subtitle = my_label)+
   theme(plot.background = element_blank())
 p
-ggsave("./MR_Protein_vs_FinngenR12_volcano.pdf", p, width = 12, height = 10)
-####################################################################################
-mr.or <-fread('mr_result_exposure.cis-pQTLs_protein_outcome.ieu4915.bile.duct.cancer_addOR.xls',header = T)
+ggsave("final results/Figure2A_MR_Protein_vs_FinngenR12_volcano.pdf", p, width = 12, height = 10)
+######################################################################################################
+mr.or <-fread('final results/Validation cohort 1/mr_result_exposure.cis-pQTLs_protein_outcome.ieu4915.bile.duct.cancer_addOR.xls',header = T)
 mr.or <- mr.or[mr.or$method!="Weighted median",]
 mr.or <- mr.or[mr.or$method!="MR Egger",]
 
@@ -66,6 +65,44 @@ sig.res <- res %>% mutate(group=case_when(
   (pval < 0.05 & or < 1) ~ "Negatively associated",
   .default = "Not significant"))
 
+label <-c("NCAN")
+my_label <- paste0( "P<0.05 ; ",
+                    "Positively associated:",table(sig.res$group)[1]," ; ",
+                    "Negatively associated:",table(sig.res$group)[2])
+p <- ggscatter(sig.res,
+               x = "OR", y = "logP",
+               label = "id.exposure",
+               label.select = label,
+               color = "group", size = 2,
+               main = paste0("Replication cohort 1(IEU--b-4915)"), # ***
+               xlab = "OR", ylab = "-log10(P.Value)",
+               palette = c("#D01910","#00599F","#CCCCCC"),
+               ylim = c(-0.1, 7),xlim=c(0.995,1.005))+
+  theme_base()+
+  geom_hline(yintercept = -log10(0.05), linetype="dashed", color = "#222222") +
+  geom_vline(xintercept = 1 , linetype="dashed", color = "#222222") +
+  #geom_vline(xintercept = -1, linetype="dashed", color = "#222222") +
+  labs(subtitle = my_label)+
+  theme(plot.background = element_blank())
+
+ggsave("final results/Figure2B_MR_Protein_vs_IEU-b-4915_volcano.pdf", p, width = 12, height = 10)
+##############################################################################################################
+mr.or <-fread('final results/Validation cohort 2/mr_result_exposure.cis-pQTLs_protein_outcome.g3859.bile.duct.cancer_addOR.xls',header = T)
+
+mr.or <- mr.or[mr.or$method!="Weighted median",]
+mr.or <- mr.or[mr.or$method!="MR Egger",]
+
+res <- mr.or %>% filter(!is.na(pval)) %>% 
+  mutate( logP = -log10(pval) ) %>%
+  mutate( OR = or ) %>%
+  mutate( tag = "Discovery Bile Duct Cancer")%>%
+  mutate( Gene = rownames(id.exposure))
+sig.res <- res %>% mutate(group=case_when(
+  (pval < 0.05 & or > 1) ~ "Positively associated",
+  (pval < 0.05 & or < 1) ~ "Negatively associated",
+  .default = "Not significant"))
+
+label <-c("SERPINA1")
 my_label <- paste0( "P<0.05 ; ",
                     "Positively associated:",table(sig.res$group)[1]," ; ",
                     "Negatively associated:",table(sig.res$group)[2])
@@ -86,42 +123,9 @@ p <- ggscatter(sig.res,
   theme(plot.background = element_blank())
 p
 
-ggsave("./MR_Protein_vs_GCST90043859_volcano.pdf", p, width = 12, height = 10)
+ggsave("final results/Figure2C_MR_Protein_vs_GCST90043859_volcano.pdf", p, width = 12, height = 10)
 
 
-######################################################################################################
-mr.or <-fread('mr_result_exposure.cis-pQTLs_protein_outcome.g3859.bile.duct.cancer_addOR.xls',header = T)
-mr.or <- mr.or[mr.or$method!="Weighted median",]
-mr.or <- mr.or[mr.or$method!="MR Egger",]
 
-res <- mr.or %>% filter(!is.na(pval)) %>% 
-  mutate( logP = -log10(pval) ) %>%
-  mutate( OR = or ) %>%
-  mutate( tag = "Discovery Bile Duct Cancer")%>%
-  mutate( Gene = rownames(id.exposure))
-sig.res <- res %>% mutate(group=case_when(
-  (pval < 0.05 & or > 1) ~ "Positively associated",
-  (pval < 0.05 & or < 1) ~ "Negatively associated",
-  .default = "Not significant"))
-my_label <- paste0( "P<0.05 ; ",
-                    "Positively associated:",table(sig.res$group)[1]," ; ",
-                    "Negatively associated:",table(sig.res$group)[2])
-p <- ggscatter(sig.res,
-               x = "OR", y = "logP",
-               label = "id.exposure",
-               label.select = label,
-               color = "group", size = 2,
-               main = paste0("Validation Bile Duct Cancer"), # ***
-               xlab = "OR", ylab = "-log10(P.Value)",
-               palette = c("#D01910","#00599F","#CCCCCC"),
-               ylim = c(-0.1, 7),xlim=c(0.995,1.005))+
-  theme_base()+
-  geom_hline(yintercept = -log10(0.05), linetype="dashed", color = "#222222") +
-  geom_vline(xintercept = 1 , linetype="dashed", color = "#222222") +
-  #geom_vline(xintercept = -1, linetype="dashed", color = "#222222") +
-  labs(subtitle = my_label)+
-  theme(plot.background = element_blank())
-
-ggsave("./MR_Protein_vs_IEU-b-4915_volcano.pdf", p, width = 12, height = 10)
 
 

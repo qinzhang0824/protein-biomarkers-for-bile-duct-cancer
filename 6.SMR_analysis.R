25a4b3bf194d4531e8dabc0e7ea3489d43205e84
qinzhang0824 <52529819+qinzhang0824@users.noreply.github.com>
2026-01-18 13:00:59 +0800

    Update and rename 6.SMR_analysis.sh to 6.SMR_analysis.R

---
+++ 6.SMR_analysis.R
@@ -0,0 +1,11 @@
+#1.step  gwas
+g <- fread ("/finngen_R12_C3_Bile_outcome.MR.format.xls",sep='\t',header = T)
+
+f<-select(g,SNP,effect_allele.outcome,other_allele.outcome,eaf.outcome,beta.outcome,se.outcome,pval.outcome,samplesize.outcome)
+colnames(f)<-c("SNP","A1","A2","freq","b","se","p","n")
+
+f_clean <- f[!apply(f, 1, function(row) {any(is.na(row) | row == "")}), , drop = FALSE]
+
+f_clean[, "SNP"] <- sub(",.*", "", f[, "SNP"])
+
+export(f_clean,"SMR_GWAS_FinngenR12_input.txt",format = "\t")

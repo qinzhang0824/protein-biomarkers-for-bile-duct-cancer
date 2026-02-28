33995c967121cca5610093bbbf03928c16aee367
qinzhang0824 <52529819+qinzhang0824@users.noreply.github.com>
2026-02-28 15:59:36 +0800

    Update file path for input data export

--- 6_SMR_analysis.R
+++ 6_SMR_analysis.R
@@ -5,7 +5,7 @@ library(readxl)
 library(plyr)
 
 
-g <- fread ("/finngen_R12_C3_Bile_outcome.MR.format.xls",sep='\t',header = T)
+g <- fread ("intermediate_files/finngen_R12_C3_Bile_outcome.MR.format.xls",sep='\t',header = T)
 
 f<-select(g,SNP,effect_allele.outcome,other_allele.outcome,eaf.outcome,beta.outcome,se.outcome,pval.outcome,samplesize.outcome)
 colnames(f)<-c("SNP","A1","A2","freq","b","se","p","n")
@@ -14,4 +14,4 @@ f_clean <- f[!apply(f, 1, function(row) {any(is.na(row) | row == "")}), , drop =
 
 f_clean[, "SNP"] <- sub(",.*", "", f[, "SNP"])
 
-export(f_clean,"SMR_GWAS_FinngenR12_input.txt",format = "\t")
+export(f_clean,"SMR_analysis/SMR_GWAS_FinngenR12_input.txt",format = "\t")

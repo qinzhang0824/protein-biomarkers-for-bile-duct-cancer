edefbcbcd57dc6505cafd15b9e7dc2d9a1662750
qinzhang0824 <52529819+qinzhang0824@users.noreply.github.com>
2026-02-28 23:39:01 +0800

    Enhance analysis workflow with new libraries and plots
    Added libraries for analysis and created multiple plots for MR analysis results, including Figures 2, 3, and 4. Implemented data filtering and transformation for visualization and saved outputs.

--- analysis_workflow.R
+++ analysis_workflow.R
@@ -18,9 +18,14 @@ library(S4Vectors)
 library(rio)
 library(ggplot2)
 library(ggsci)
-library("ggpubr")
 library(forestplot)
 library(VariantAnnotation)
+library(limma)
+library(tidyverse)
+library(openxlsx)
+library(ggpubr)
+library(ggthemes)
+library(grid)
 
 ############################################################################################################ read Instruments variables of plasma proteins
 protein <- fread("Raw_input_data/Instrumental_variables_of_plasma_proteins_used_in_MR_analysis.txt",sep='\t')
@@ -126,3 +131,290 @@ export(MR.OR,"Final_results/Validation_cohort_2/mr_result_exposure.cis-pQTLs_pro
 
 mr_steiger_direction <-directionality_test(mydata)
 export(mr_steiger_direction,"Final_results/Validation_cohort_2/mr_result_exposure.cis-PlasmaProtein_g3859_steiger_direction.xls",format = "\t")
+
+################################################################################################ Figure2 plot
+
+mr.or <-fread('final results/Discovery cohort/mr_result_exposure.cis-pQTLs_protein_outcome.finngen.R12.bile.duct.cancer_addOR.xls',header = T)
+mr.or <- mr.or[mr.or$method!="Weighted median",]
+mr.or <- mr.or[mr.or$method!="MR Egger",]
+
+res <- mr.or %>% filter(!is.na(pval)) %>% 
+  mutate( logP = -log10(pval) ) %>%
+  mutate( OR = or ) %>%
+  mutate( tag = "Validation Bile Duct Cancer")%>%
+  mutate( Gene = rownames(id.exposure))
+
+sig.res <- res %>% mutate(group=case_when(
+  (pval < 0.05/1193 & or > 1) ~ "Positively associated",
+  (pval < 0.05/1193 & or < 1) ~ "Negatively associated",
+  .default = "Not significant"))
+
+
+sig.res <- sig.res %>% mutate(group=factor(group,levels = c("Positively associated","Negatively associated","Not significant")))
+
+label <-subset(sig.res$id.exposure,sig.res$pval < (0.05/1193))
+label <-subset(sig.res$id.exposure,sig.res$id.exposure %in% c("NCAN"))
+label <-subset(sig.res$id.exposure,sig.res$id.exposure %in% c("SERPINA1"))
+label <-c("NCAN","SERPINA1")
+
+my_label <- paste0( "P<4.19 x 10-5 ; ",
+                    "Positively associated:",table(sig.res$group)[1]," ; ",
+                    "Negatively associated:",table(sig.res$group)[2])
+p <- ggscatter(sig.res,
+               x = "OR", y = "logP",
+               label = "id.exposure",
+               label.select = label,
+               color = "group", size = 2,
+               main = paste0("Discovery cohort(FinngenR12)"), # ***
+               xlab = "OR", ylab = "-log10(P.Value)",
+               palette = c("#D01910","#00599F","#CCCCCC"),
+               ylim = c(-0.1,5),xlim=c(0,3.0))+
+  theme_base()+
+  geom_hline(yintercept = -log10(0.05/1193), linetype="dashed", color = "#222222") +
+  geom_vline(xintercept = 1 , linetype="dashed", color = "#222222") +
+  #geom_vline(xintercept = -1, linetype="dashed", color = "#222222") +
+  labs(subtitle = my_label)+
+  theme(plot.background = element_blank())
+p
+ggsave("final results/Figure2A_MR_Protein_vs_FinngenR12_volcano.pdf", p, width = 12, height = 10)
+##########################################################################
+mr.or <-fread('final results/Validation cohort 1/mr_result_exposure.cis-pQTLs_protein_outcome.ieu4915.bile.duct.cancer_addOR.xls',header = T)
+mr.or <- mr.or[mr.or$method!="Weighted median",]
+mr.or <- mr.or[mr.or$method!="MR Egger",]
+
+res <- mr.or %>% filter(!is.na(pval)) %>% 
+  mutate( logP = -log10(pval) ) %>%
+  mutate( OR = or ) %>%
+  mutate( tag = "Discovery Bile Duct Cancer")%>%
+  mutate( Gene = rownames(id.exposure))
+sig.res <- res %>% mutate(group=case_when(
+  (pval < 0.05 & or > 1) ~ "Positively associated",
+  (pval < 0.05 & or < 1) ~ "Negatively associated",
+  .default = "Not significant"))
+
+label <-c("NCAN")
+my_label <- paste0( "P<0.05 ; ",
+                    "Positively associated:",table(sig.res$group)[1]," ; ",
+                    "Negatively associated:",table(sig.res$group)[2])
+p <- ggscatter(sig.res,
+               x = "OR", y = "logP",
+               label = "id.exposure",
+               label.select = label,
+               color = "group", size = 2,
+               main = paste0("Replication cohort 1(IEU--b-4915)"), # ***
+               xlab = "OR", ylab = "-log10(P.Value)",
+               palette = c("#D01910","#00599F","#CCCCCC"),
+               ylim = c(-0.1, 7),xlim=c(0.995,1.005))+
+  theme_base()+
+  geom_hline(yintercept = -log10(0.05), linetype="dashed", color = "#222222") +
+  geom_vline(xintercept = 1 , linetype="dashed", color = "#222222") +
+  #geom_vline(xintercept = -1, linetype="dashed", color = "#222222") +
+  labs(subtitle = my_label)+
+  theme(plot.background = element_blank())
+
+ggsave("final results/Figure2B_MR_Protein_vs_IEU-b-4915_volcano.pdf", p, width = 12, height = 10)
+###########################################################
+mr.or <-fread('final results/Validation cohort 2/mr_result_exposure.cis-pQTLs_protein_outcome.g3859.bile.duct.cancer_addOR.xls',header = T)
+
+mr.or <- mr.or[mr.or$method!="Weighted median",]
+mr.or <- mr.or[mr.or$method!="MR Egger",]
+
+res <- mr.or %>% filter(!is.na(pval)) %>% 
+  mutate( logP = -log10(pval) ) %>%
+  mutate( OR = or ) %>%
+  mutate( tag = "Discovery Bile Duct Cancer")%>%
+  mutate( Gene = rownames(id.exposure))
+sig.res <- res %>% mutate(group=case_when(
+  (pval < 0.05 & or > 1) ~ "Positively associated",
+  (pval < 0.05 & or < 1) ~ "Negatively associated",
+  .default = "Not significant"))
+
+label <-c("SERPINA1")
+my_label <- paste0( "P<0.05 ; ",
+                    "Positively associated:",table(sig.res$group)[1]," ; ",
+                    "Negatively associated:",table(sig.res$group)[2])
+p <- ggscatter(sig.res,
+               x = "OR", y = "logP",
+               label = "id.exposure",
+               label.select = label,
+               color = "group", size = 2,
+               main = paste0("Replication cohort2(GCST90043859)"), # ***
+               xlab = "OR", ylab = "-log10(P.Value)",
+               palette = c("#D01910","#00599F","#CCCCCC"),
+               ylim = c(-0.1,5),xlim=c(0,3.0))+
+  theme_base()+
+  geom_hline(yintercept = -log10(0.05/1193), linetype="dashed", color = "#222222") +
+  geom_vline(xintercept = 1 , linetype="dashed", color = "#222222") +
+  #geom_vline(xintercept = -1, linetype="dashed", color = "#222222") +
+  labs(subtitle = my_label)+
+  theme(plot.background = element_blank())
+p
+
+ggsave("final results/Figure2C_MR_Protein_vs_GCST90043859_volcano.pdf", p, width = 12, height = 10)
+
+####################################################################################################### Figure3 plot
+mr.or <-fread("final results/Discovery cohort/mr_result_exposure.cis-pQTLs_protein_outcome.finngen.R12.bile.duct.cancer_addOR.xls",sep='\t',header=T)
+mr.or1 <-fread('final results/Validation cohort 1/mr_result_exposure.cis-pQTLs_protein_outcome.ieu4915.bile.duct.cancer_addOR.xls',header = T)
+mr.or2 <-fread('final results/Validation cohort 2/mr_result_exposure.cis-pQTLs_protein_outcome.g3859.bile.duct.cancer_addOR.xls',header = T)
+
+out_multi.finn <-mr.or[mr.or$pval<(0.05/1193),]
+out_multi.4915 <-mr.or1[mr.or1$id.exposure %in% c("NCAN","SERPINA1"),]
+out_multi.g3859 <-mr.or2[mr.or2$id.exposure %in% c("NCAN","SERPINA1"),]
+
+out_multi <-rbind(out_multi.finn,out_multi.4915,out_multi.g3859)
+
+hz <- paste(round(out_multi$or,3),
+            "(",round(out_multi$or_lci95,3),
+            "-",round(out_multi$or_uci95,3),")",sep = "")
+
+
+tabletext <- cbind(c(NA,"Exposure",out_multi$id.exposure),
+                   c(NA,"Outcome",out_multi$outcome),
+                   c(NA,"Methods",out_multi$method),
+                   c(NA,"P value",ifelse(out_multi$pval<0.001,"P < 0.001",round(out_multi$pval,3))),
+                   #c(NA,"P value",out_multi$pval),
+                   c(NA,"OR(95% CI)",hz))
+
+
+pforest <- forestplot(labeltext=tabletext, 
+                      graph.pos=4,
+                      col=fpColors(box="#00468B", lines="black", zero = "red"),
+                      mean=c(NA,NA,out_multi$or),
+                      lower=c(NA,NA,out_multi$or_lci95), 
+                      upper=c(NA,NA,out_multi$or_uci95), 
+                      xlab="Odds Ratio(95%CI)",
+                      boxsize=0.2,lwd.ci=0.8,  
+                      ci.vertices.height = 0.08,ci.vertices=TRUE,
+                      zero=1,lwd.zero=1,     
+                      colgap=unit(14,"mm"),   
+                      xticks = c(0.3,0.5,0.6,0.7,0.8,0.9,1,1.1,1.3,1.5,5,7), 
+                      lwd.xaxis=2,         
+                      lineheight = unit(1,"cm"), 
+                      graphwidth = unit(0.2,"npc"), 
+                      #cex=0.9, fn.ci_norm = fpDrawCircleCI, 
+                      hrzl_lines=list("2" = gpar(lwd=2, col="black"),
+                                      "3" = gpar(lwd=2, col="black")),           
+                      clip=c(0,3),
+                      txt_gp=fpTxtGp(label=gpar(cex=1.25),
+                                     ticks=gpar(cex=1.1),
+                                     xlab=gpar(cex = 1.4),
+                                     title=gpar(cex = 1.2))
+)
+pdf("final results/Figure3_Plasma_Protein_vs_Discovery_and_validation_Forest.pdf", width=12, height=6)
+pforest
+dev.off()
+######################################################################################################### Figure4 plot 
+#####################################################################  NCAN
+t.chr <-'19'
+t.pos <-19329924
+
+out <- fread ("/intermediate_files/finngen_R12_C3_Bile_outcome.MR.format_NCAN.xls",sep='\t',header = T)
+gwas <- out[out$chr.outcome=="19",]
+gwas <- gwas[gwas$pos.outcome >t.pos-1000000 & gwas$pos.outcome < t.pos+1000000,]
+
+protein <- fread("/Raw_input_data/Ferkingstad.15573_110_NCAN_CSPG3.txt",sep='\t')
+protein$phenotype <- "Proteins"
+protein <- as.data.frame(protein)
+data <- format_data(protein,type = "exposure",
+                    header = TRUE,
+                    phenotype_col = "phenotype",
+                    snp_col = "rsids",
+                    beta_col = "Beta",
+                    se_col = "SE",
+                    eaf_col = "ImpMAF",
+                    effect_allele_col = "effectAllele",
+                    other_allele_col = "otherAllele",
+                    pval_col = "Pval",
+                    chr_col = "Chrom",
+                    samplesize_col = "N",
+                    id_col = "Proteins",
+                    pos_col = "Pos")
+pQTL <-data[data$chr.exposure=="chr19",]
+pQTL <- pQTL[pQTL$pos.exposure >t.pos-1000000 & pQTL$pos.exposure < t.pos+1000000,]
+
+########## finngen case 2298
+gwas$s <-as.numeric(2298/gwas$samplesize.outcome)
+gwas$MAF <- ifelse(gwas$eaf.outcome<0.5,gwas$eaf.outcome,1-gwas$eaf.outcome)
+pQTL$MAF <- ifelse(pQTL$eaf.exposure<0.5,pQTL$eaf.exposure,1-pQTL$eaf.exposure)
+
+gwas <- gwas[gwas$MAF != 'NA',]
+pQTL <- pQTL[pQTL$MAF !='NA',]
+
+sameSNP <- intersect(pQTL$SNP,gwas$SNP)
+
+pQTL <- pQTL[pQTL$SNP %in% sameSNP,]
+gwas <- gwas[gwas$SNP %in% sameSNP,]
+
+result <- coloc.abf(dataset1=list(pvalues=gwas$pval.outcome, snp=gwas$SNP,MAF=gwas$MAF,beta=gwas$beta.outcome, varbeta=gwas$se.outcome^2,type="cc", s=gwas$s[1], N=gwas$samplesize.outcome),dataset2=list(pvalues=pQTL$pval.exposure, snp=pQTL$SNP,MAF=pQTL$MAF,beta=pQTL$beta.exposure, varbeta=pQTL$se.exposure^2, type="quant", N=pQTL$samplesize.exposure), MAF=pQTL$MAF)
+
+gwas_fn <- gwas[,c('SNP','pval.outcome')] %>% dplyr::rename(rsid = SNP, pval = pval.outcome)
+pQTL_fn <- pQTL[,c('SNP','pval.exposure')] %>% dplyr::rename(rsid = SNP, pval = pval.exposure)
+
+locuscompare(in_fn1 =pQTL_fn , in_fn2 = gwas_fn, title1 = 'pQTL',title2 = 'GWAS',snp="rs2228603")
+ggsave('/final_results/Figure4_NCAN_finngen_Coloc.png',width=9,height = 4.5)
+
+####################################################################### SERPINA1
+
+t.chr <-'14' ###finngen protein
+t.pos <-94844947
+
+out <- fread ("/intermediate_files/finngen_R12_C3_Bile_outcome.MR.format_SERPINA1.xls",sep='\t',header = T)
+gwas <- out[out$chr.outcome=="14",]
+gwas <- gwas[gwas$pos.outcome >t.pos-1000000 & gwas$pos.outcome < t.pos+1000000,]
+
+protein <- fread("/Raw_input_data/Pietzner.a1_Antitrypsin_3580_25.txt",sep='\t')
+protein$phenotype <- "Proteins"
+protein <- as.data.frame(protein)
+data <- format_data(protein,type = "exposure",
+                    header = TRUE,
+                    phenotype_col = "phenotype",
+                    snp_col = "rsid",
+                    beta_col = "Effect",
+                    se_col = "StdErr",
+                    eaf_col = "MinFreq",
+                    effect_allele_col = "Allele1",
+                    other_allele_col = "Allele2",
+                    pval_col = "Pvalue",
+                    chr_col = "chr",
+                    samplesize_col = "TotalSampleSize",
+                    id_col = "Proteins",
+                    pos_col = "pos")
+pQTL <-data[data$chr.exposure=="chr14",]
+pQTL <- pQTL[pQTL$pos.exposure >t.pos-1000000 & pQTL$pos.exposure < t.pos+1000000,]
+
+gwas$MAF <- ifelse(gwas$eaf.outcome<0.5,gwas$eaf.outcome,1-gwas$eaf.outcome)
+pQTL$MAF <- ifelse(pQTL$eaf.exposure<0.5,pQTL$eaf.exposure,1-pQTL$eaf.exposure)
+
+gwas <- gwas[gwas$MAF != 'NA',]
+pQTL <- pQTL[pQTL$MAF !='NA',]
+
+sameSNP <- intersect(pQTL$SNP,gwas$SNP)
+
+pQTL <- pQTL[pQTL$SNP %in% sameSNP,]
+gwas <- gwas[gwas$SNP %in% sameSNP,]
+
+##### finngen
+gwas$s <-as.numeric(2298/gwas$samplesize.outcome)
+
+result <- coloc.abf(dataset1=list(pvalues=gwas$pval.outcome, snp=gwas$SNP,MAF=gwas$MAF,beta=gwas$beta.outcome, varbeta=gwas$se.outcome^2,type="cc", s=gwas$s[1], N=gwas$samplesize.outcome),dataset2=list(pvalues=pQTL$pval.exposure, snp=pQTL$SNP,MAF=pQTL$MAF,beta=pQTL$beta.exposure, varbeta=pQTL$se.exposure^2, type="quant", N=pQTL$samplesize.exposure), MAF=pQTL$MAF)
+
+gwas_fn <- gwas[,c('SNP','pval.outcome')] %>% dplyr::rename(rsid = SNP, pval = pval.outcome)
+pQTL_fn <- pQTL[,c('SNP','pval.exposure')] %>% dplyr::rename(rsid = SNP, pval = pval.exposure)
+
+locuscompare(in_fn1 =pQTL_fn , in_fn2 = gwas_fn, title1 = 'pQTL',title2 = 'GWAS',snp="rs28929474")
+
+ggsave('/final_results/Figure4_SERPINA1_finngen_Coloc.png',width=9,height = 4.5)
+###############################################################################################################################  SMR input
+
+g <- fread ("intermediate_files/finngen_R12_C3_Bile_outcome.MR.format.xls",sep='\t',header = T)
+
+f<-select(g,SNP,effect_allele.outcome,other_allele.outcome,eaf.outcome,beta.outcome,se.outcome,pval.outcome,samplesize.outcome)
+colnames(f)<-c("SNP","A1","A2","freq","b","se","p","n")
+
+f_clean <- f[!apply(f, 1, function(row) {any(is.na(row) | row == "")}), , drop = FALSE]
+
+f_clean[, "SNP"] <- sub(",.*", "", f[, "SNP"])
+
+export(f_clean,"SMR_analysis/SMR_GWAS_FinngenR12_input.txt",format = "\t")
+
+
